5985ef6f15f4df585ab34d2515606c3173826113
Parra14 <ofparra69@gmail.com>
2024-06-08 12:57:47 -0500

    *

--- Frontend/src/app/components/inmuebles/create-inmueble/create-inmueble.component.ts
+++ Frontend/src/app/components/inmuebles/create-inmueble/create-inmueble.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { InmuebleServicesService } from 'src/app/services/inmueble-services.service';
+import { Inmueble } from 'src/app/models/inmueble';
+
 
 @Component({
   selector: 'app-create-inmueble',
@@ -7,9 +12,66 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateInmuebleComponent implements OnInit {
 
-  constructor() { }
+  inmuebleForm: FormGroup;
+
+
+  constructor(private fb: FormBuilder,
+    private inmuebleServicesService: InmuebleServicesService,
+    private router: Router) {
+      this.inmuebleForm = this.fb.group({
+        area: ['', Validators.required],
+        direccion: ['', Validators.required],
+        estancias: this.fb.array([]),
+        caracteristicas: this.fb.array([]),
+        zona: ['', Validators.required],
+        oferta: ['', Validators.required],
+        valor: ['', Validators.required],
+        nombrePropietario: ['', Validators.required],
+        telefonoPropietario: ['', Validators.required]
+      });
+     }
 
   ngOnInit(): void {
+  }
+
+  get estancias(): FormArray {
+    return this.inmuebleForm.get('estancias') as FormArray;
+  }
+
+  get caracteristicas(): FormArray {
+    return this.inmuebleForm.get('caracteristicas') as FormArray;
+  }
+
+  addEstancia(): void {
+    const estanciaForm = this.fb.group({
+      descripcion: ['', Validators.required],
+      cantidad: ['', Validators.required]
+    });
+    this.estancias.push(estanciaForm);
+  }
+
+  removeEstancia(index: number): void {
+    this.estancias.removeAt(index);
+  }
+
+  addCaracteristica(): void {
+    const caracteristicaForm = this.fb.group({
+      descripcion: ['', Validators.required]
+    });
+    this.caracteristicas.push(caracteristicaForm);
+  }
+
+  removeCaracteristica(index: number): void {
+    this.caracteristicas.removeAt(index);
+  }
+
+  agregarInmueble(): void {
+    if (this.inmuebleForm.valid) {
+      const nuevoInmueble: Inmueble = this.inmuebleForm.value;
+      this.inmuebleServicesService.guardarInmueble(nuevoInmueble).subscribe(() => {
+        this.router.navigate(['/listar-inmueble']);
+      });
+    }
   }
 
 }
